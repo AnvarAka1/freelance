eca6e5df6f7335c7e49675d35f3eadf88108edb5
Anvar <Anvar-abd_97@mail.ru>
2019-06-21 03:46:44 +0500

    Toolbar almost done 2

--- src/components/Navigation/SearchField/SearchField.js
+++ src/components/Navigation/SearchField/SearchField.js
@@ -1,11 +1,19 @@
 import React from "react";
 import InputBase from "@material-ui/core/InputBase";
-import SearchIcon from "@material-ui/core/Icon";
+import Icon from "@material-ui/core/Icon";
+import classes from "./SearchField.module.css";
 const searchField = props => {
   return (
-    <div>
-      <SearchIcon>star</SearchIcon>
-      <InputBase />
+    <div className={classes.SearchField}>
+      <InputBase placeholder="Search" className={classes.Input} />
+      <Icon
+        onClick={() => {
+          alert("Search");
+        }}
+        className={classes.SearchIcon}
+      >
+        search
+      </Icon>
     </div>
   );
 };
